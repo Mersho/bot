61a631fddc1c4056f9300e8a93741c81dc863871
Mehrshad <code.rezaei@gmail.com>
2024-03-13 13:45:31 +0330

    commands: inform buyer when order is canceled
    When the seller cancels an order, the buyer will get a wrong
    and confusing message:
    
    ```
    You have cancelled the order ID: orderID
    ```
    
    So, in this commit, the bot sends a message that
    tells the buyer:
    
    ```
    Order orderId has been cancelled. There is no need to create an invoice.
    ```
    
    Fixes: https://github.com/lnp2pBot/bot/issues/482

--- bot/messages.ts
+++ bot/messages.ts
@@ -977,6 +977,17 @@ const successCancelOrderMessage = async (ctx: MainContext, user: UserDocument, o
   }
 };
 
+const successCancelOrderInvoiceMessage = async (ctx: MainContext, user: UserDocument, order: IOrder, i18n: I18nContext) => {
+  try {
+    await ctx.telegram.sendMessage(
+      user.tg_id,
+      i18n.t('cancel_invoice', { orderId: order._id })
+    );
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
 const counterPartyCancelOrderMessage = async (ctx: MainContext, user: UserDocument, order: IOrder, i18n: I18nContext) => {
   try {
     await ctx.telegram.sendMessage(
@@ -1680,6 +1691,7 @@ module.exports = {
   successCompleteOrderMessage,
   successCancelOrderByAdminMessage,
   successCancelOrderMessage,
+  successCancelOrderInvoiceMessage,
   badStatusOnCancelOrderMessage,
   invoicePaymentFailedMessage,
   userCantTakeMoreThanOneWaitingOrderMessage,
