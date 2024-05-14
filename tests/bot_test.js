2fc63744730d4b527e606491aab95a179383e760
Mehrshad <code.rezaei@gmail.com>
2024-05-14 18:53:41 +0330

    WIP

--- tests/bot_test.js
+++ tests/bot_test.js
@@ -115,6 +115,7 @@ describe('Telegram bot test', () => {
     const res = await client.sendCommand(command);
     expect(res.ok).to.be.equal(true);
     const updates = await client.getUpdates();
+    console.log("updates:");
     console.log(updates);
     console.log(updates.result[0]);
     userStub.restore();
