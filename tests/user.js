fad25f49aad2b557011972bac576ecb639deee7a
Mehrshad <code.rezaei@gmail.com>
2024-05-14 19:39:14 +0330

    WIP: update test model

--- tests/user.js
+++ tests/user.js
@@ -1,15 +1,20 @@
 const user = {
-  lang: 'ESP',
-  trades_completed: 7,
-  admin: false,
-  banned: false,
-  disputes: 0,
-  _id: '61006f0e85ad4f96cde94141',
-  balance: 0,
-  tg_id: '1',
-  username: 'negrunch',
-  created_at: '2021-07-27T20:39:42.403Z',
-};
+  "tg_id": "1",
+  "username": "testUserName",
+  "lang": "en",
+  "trades_completed": 0,
+  "total_reviews": 0,
+  "last_rating": 0,
+  "total_rating": 0,
+  "volume_traded": 0,
+  "admin": false,
+  "banned": false,
+  "show_username": false,
+  "show_volume_traded": false,
+  "disputes": 0,
+  "reviews": [],
+  "created_at": "2024-05-14T15:46:29.058Z",
+}
 
 user.save = () => {};
 
